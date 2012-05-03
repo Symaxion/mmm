95acc95211531fb8a1679ec0a6d4a1f919cedf4f
Frank Erens <frank@synthi.net>
2012-05-03 10:44:02 +0200

    Initial GUI implementation.

--- src/gui.h
+++ src/gui.h
@@ -1,7 +1,22 @@
 #include <QtGui/QWidget>
 
+class QVBoxLayout;
+class QHBoxLayout;
+class QListWidget;
+class QPushButton;
+
 class Gui : public QWidget {
     Q_OBJECT
 public:
     Gui(QWidget* parent = 0); 
+
+private:
+    QVBoxLayout* mLayout;
+    QListWidget* mListWidget;
+    QWidget* mButtonContainer;
+    QHBoxLayout* mButtonLayout;
+    QPushButton* mAddButton;
+    QPushButton* mRemoveButton;
+    QPushButton* mOpenButton;
+    QPushButton* mLaunchButton;
 };
