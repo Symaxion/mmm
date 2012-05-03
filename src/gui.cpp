1311ee7755fb094312c5c3b3a46f2c44c0fc2851
Frank "SeySayux" Erens <seysayux@users.sourceforge.net>
2012-05-03 10:44:02 +0200

    Initial GUI implementation.

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,5 +1,44 @@
 #include "gui.h"
 
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QListWidget>
+#include <QtGui/QPushButton>
+
 Gui::Gui(QWidget* parent) : QWidget(parent) {
-    
+    this->setWindowTitle("Multi Minecraft Manager");
+    this->resize(384, 420);
+
+    mLayout = new QVBoxLayout(this);
+    #ifndef Q_OS_MAC
+    mLayout->setSpacing(6);
+    #endif
+    mLayout->setContentsMargins(6, 6, 6, 6);
+
+    mListWidget = new QListWidget(this);
+    mLayout->addWidget(mListWidget);
+
+    mButtonContainer = new QWidget(this);
+
+    mButtonLayout = new QHBoxLayout(mButtonContainer);
+    mButtonLayout->setSpacing(1);
+    mButtonLayout->setContentsMargins(1, 1, 1, 1);
+
+    mAddButton = new QPushButton(mButtonContainer);
+    mAddButton->setText("Add...");
+    mButtonLayout->addWidget(mAddButton);
+
+    mRemoveButton = new QPushButton(mButtonContainer);
+    mRemoveButton->setText("Remove");
+    mButtonLayout->addWidget(mRemoveButton);
+
+    mOpenButton = new QPushButton(mButtonContainer);
+    mOpenButton->setText("Open Folder");
+    mButtonLayout->addWidget(mOpenButton);
+
+    mLaunchButton = new QPushButton(mButtonContainer);
+    mLaunchButton->setText("Launch");
+    mButtonLayout->addWidget(mLaunchButton);
+
+    mLayout->addWidget(mButtonContainer);
 } 
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
 #include <QtGui/QFileDialog>
 
 #include "os.h"
+#include "gui.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    QWidget w;
-
-    w.show();
-
     if(!OS::isInitialized()) {
         OS::initialize();
     }
+
+    Gui w;
+    w.show();
 
     return app.exec();
 }
